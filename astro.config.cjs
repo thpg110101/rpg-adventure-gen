85b040b3ea73f0f63e65f6b79107a4d6c2a8b604
thpg110101 <thpg110101@gmail.com>
2026-03-12 13:21:12 -0500

    Update astro.config.cjs

--- astro.config.cjs
+++ astro.config.cjs
@@ -1,8 +1,11 @@
-module.exports = {
+import { defineConfig } from 'astro.config'
+
+export default defineConfig({
   output: 'static',
   // Replace 'your-username' with your actual GitHub username
   site: 'https://thpg110101.github.io',
   // Replace 'your-repo-name' with the name of the repository you created
   base: '/rpg-adventure-gen',
 
-};
+})
+
